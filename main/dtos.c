20bb852cf8a554374e02b3c905a00991417fe6cc
Wesley Santos <BINARY_QUANTUM@outlook.com>
2022-11-02 08:42:50 -0300

    feat: dtos

--- main/dtos.c
+++ main/dtos.c
@@ -14,25 +14,6 @@
 
 static void cJSON_ToString(char *str, cJSON *obj);
 
-void wifi_sta_config_from_json(wifi_sta_config_t *data, char *json_str)
-{
-    cJSON *json = cJSON_Parse(json_str);
-    cJSON *ssid = cJSON_GetObjectItem(json, "ssid");
-    cJSON *password = cJSON_GetObjectItem(json, "password");
-    memset(data, 0, sizeof(*data));
-    if(ssid && ssid->valuestring[0] != '\0') strncpy((char*)data->ssid, ssid->valuestring, sizeof(data->ssid));
-    if(password && password->valuestring[0] != '\0') strncpy((char*)data->password, password->valuestring, sizeof(data->password));
-    cJSON_Delete(json);
-}
-
-void wifi_sta_config_to_json(char *json_str, wifi_sta_config_t *data)
-{
-    cJSON *json = cJSON_CreateObject();
-    cJSON_AddStringToObject(json, "ssid", (char*)&data->ssid);
-    cJSON_AddStringToObject(json, "password", (char*)&data->password);
-    cJSON_ToString(json_str, json);
-}
-
 void net_app_wifi_sta_info_to_json(char *json_str, net_app_wifi_sta_info_t *data)
 {
     cJSON *json = cJSON_CreateObject();
@@ -53,6 +34,74 @@ void wifi_ap_record_to_json(char *json_str, wifi_ap_record_t *data, size_t size)
                        data[i].ssid, data[i].rssi, data[i].authmode);
     }
     json_str[len - 1] = ']';
+}
+
+void net_app_ip_config_from_json(net_app_ip_config_t *data, char *json_str)
+{
+    cJSON *json = cJSON_Parse(json_str);
+    cJSON *dhcp = cJSON_GetObjectItem(json, "dhcp");
+    cJSON *ip = cJSON_GetObjectItem(json, "ip");
+    cJSON *gateway = cJSON_GetObjectItem(json, "gateway");
+    cJSON *mask = cJSON_GetObjectItem(json, "mask");
+    cJSON *dns = cJSON_GetObjectItem(json, "dns");
+    data->dhcp = dhcp ? dhcp->valueint : 1;
+    if(!ip && !gateway && !mask)
+    {
+        data->dhcp = true;
+    }
+    else
+    {
+        if(ip && ip->valuestring) data->ip_info.ip.addr = ipaddr_addr(ip->valuestring);
+        if(mask && mask->valuestring) data->ip_info.netmask.addr = ipaddr_addr(mask->valuestring);
+        if(gateway && gateway->valuestring) data->ip_info.gw.addr = ipaddr_addr(gateway->valuestring);
+        data->dns_info.ip.type = IPADDR_TYPE_V4;
+        if(dns && dns->valuestring)
+        {
+            data->dns_info.ip.u_addr.ip4.addr = ipaddr_addr(dns->valuestring);
+        }
+        else if(gateway && gateway->valuestring)
+        {
+            data->dns_info.ip.u_addr.ip4.addr = ipaddr_addr(gateway->valuestring);
+        }
+    }
+    cJSON_Delete(json);
+}
+
+void net_app_ip_config_to_json(char *json_str, net_app_ip_config_t *data)
+{
+   cJSON *json = cJSON_CreateObject();
+   cJSON_AddNumberToObject(json, "dhcp", data->dhcp);
+    if(!data->dhcp)
+    {
+        char ip[16], mask[16], gateway[16], dns[16];
+        sprintf(ip, IPSTR, IP2STR(&data->ip_info.ip));
+        sprintf(mask, IPSTR, IP2STR(&data->ip_info.netmask));
+        sprintf(gateway, IPSTR, IP2STR(&data->ip_info.gw));
+        sprintf(dns, IPSTR, IP2STR(&data->dns_info.ip.u_addr.ip4));
+        if(ip[0] != '\0') cJSON_AddStringToObject(json, "ip", ip);
+        if(mask[0] != '\0') cJSON_AddStringToObject(json, "mask", mask);
+        if(gateway[0] != '\0') cJSON_AddStringToObject(json, "gateway", gateway);
+        if(dns[0] != '\0') cJSON_AddStringToObject(json, "dns", dns);
+    }
+}
+
+void wifi_sta_config_from_json(wifi_sta_config_t *data, char *json_str)
+{
+    cJSON *json = cJSON_Parse(json_str);
+    cJSON *ssid = cJSON_GetObjectItem(json, "ssid");
+    cJSON *password = cJSON_GetObjectItem(json, "password");
+    memset(data, 0, sizeof(*data));
+    if(ssid && ssid->valuestring[0] != '\0') strncpy((char*)data->ssid, ssid->valuestring, sizeof(data->ssid));
+    if(password && password->valuestring[0] != '\0') strncpy((char*)data->password, password->valuestring, sizeof(data->password));
+    cJSON_Delete(json);
+}
+
+void wifi_sta_config_to_json(char *json_str, wifi_sta_config_t *data)
+{
+    cJSON *json = cJSON_CreateObject();
+    cJSON_AddStringToObject(json, "ssid", (char*)&data->ssid);
+    cJSON_AddStringToObject(json, "password", (char*)&data->password);
+    cJSON_ToString(json_str, json);
 }
 
 void net_app_ntp_config_from_json(net_app_ntp_config_t *data, char*json_str)
@@ -119,12 +168,22 @@ void esp_mqtt_client_config_to_json(char *json_str, esp_mqtt_client_config_t *da
 void net_app_settings_from_json(net_app_settings_t *data, char *json_str)
 {
     cJSON *json = cJSON_Parse(json_str);
+    cJSON *ip_cfg = cJSON_GetObjectItem(json, "ip_cfg");
+    cJSON *ip_cfg_sta = cJSON_GetArrayItem(ip_cfg, NET_APP_INTERFACE_WIFI_STA);
+    cJSON *ip_cfg_ap = cJSON_GetArrayItem(ip_cfg, NET_APP_INTERFACE_WIFI_AP);
+    cJSON *ip_cfg_eth = cJSON_GetArrayItem(ip_cfg, NET_APP_INTERFACE_ETH);
     cJSON *wifi = cJSON_GetObjectItem(json, "wifi");
     cJSON *ntp = cJSON_GetObjectItem(json, "ntp");
     cJSON *mqtt = cJSON_GetObjectItem(json, "mqtt");
+    char ip_cfg_sta_json = cJSON_Print(ip_cfg_sta);
+    char ip_cfg_ap_json = cJSON_Print(ip_cfg_ap);
+    char ip_cfg_eth_json = cJSON_Print(ip_cfg_eth);
     char *wifi_json = cJSON_Print(wifi);
     char *ntp_json = cJSON_Print(ntp);
     char *mqtt_json = cJSON_Print(mqtt);
+    net_app_ip_config_from_json(&data->ip_cfg[NET_APP_INTERFACE_WIFI_STA], ip_cfg_sta_json);
+    net_app_ip_config_from_json(&data->ip_cfg[NET_APP_INTERFACE_WIFI_AP], ip_cfg_ap_json);
+    net_app_ip_config_from_json(&data->ip_cfg[NET_APP_INTERFACE_ETH], ip_cfg_eth);
     wifi_sta_config_from_json(&data->wifi_sta, wifi_json);
     net_app_ntp_config_from_json(&data->ntp, ntp_json);
     esp_mqtt_client_config_from_json(&data->mqtt, mqtt_json);
